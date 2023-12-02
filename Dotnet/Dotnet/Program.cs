c24cde51f0f763bd91f6bfd33fecaa6a731e924a
sumit2652 <sumit_21@outlook.com>
2023-12-02 22:56:16 +0000

    Try &catch added

---
+++ Dotnet/Dotnet/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Dotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Please enter number not letter or specical carracters"); ;
+            }
+            Console.ReadKey();
+        }
+    }
+}
