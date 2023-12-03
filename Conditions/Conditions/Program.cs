eaf61a982c68387d9c8210147d784187c6e58005
sumit2652 <sumit_21@outlook.com>
2023-12-03 18:09:43 +0000

    if else codndition

--- Conditions/Conditions/Program.cs
+++ Conditions/Conditions/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Conditions
+﻿using System.Threading.Channels;
+
+namespace Conditions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter temprature");
+            string temp = Console.ReadLine();
+            int numTem = int.Parse(temp);
+
+            if(numTem < 18)
+            {
+                Console.WriteLine("take a jacket, it is below 20 dgree");
+            }
+            if(numTem > 18) {
+                Console.WriteLine("Take a jumper");
+            }
+            if (numTem == 18)
+            {
+                Console.WriteLine("It is hot go for bike");
+            }
+
+            Console.Read();
         }
     }
 }
