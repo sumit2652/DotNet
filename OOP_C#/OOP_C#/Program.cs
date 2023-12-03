123f0f5829a43ccaa08362216b700468931db61a
sumit2652 <sumit_21@outlook.com>
2023-12-03 19:51:14 +0000

    code updated

--- OOP_C#/OOP_C#/Program.cs
+++ OOP_C#/OOP_C#/Program.cs
@@ -1,12 +1,25 @@
-﻿namespace OOP_C_
+﻿using System.Threading.Channels;
+
+namespace OOP_C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             car audi = new car();
-            audi.Drive();
-            audi.Stop();
+            Console.WriteLine("Press 2 to stop the car");
+            string userInput = Console.ReadLine();
+            if (userInput == "1" ) {
+                audi.Drive();
+            }
+            if (userInput == "2" )
+            {
+                audi.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Car will drive indefinetely");
+            }
         }
     }
 }
