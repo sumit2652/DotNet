71b0d72a60e6a375815e611331faa63e2c84892e
sumit2652 <sumit_21@outlook.com>
2023-12-26 21:57:58 +0000

    Internetconnection check

---
+++ internetConnetcion/internetConnetcion/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        var connectionStatus = CheckInternetConnection();
+
+        switch (connectionStatus)
+        {
+            case InternetConnectionStatus.Connected:
+                Console.WriteLine("Internet is connected.");
+                break;
+            case InternetConnectionStatus.NotConnected:
+                Console.WriteLine("No internet connection.");
+                break;
+            case InternetConnectionStatus.Unknown:
+                Console.WriteLine("Unable to determine internet connection status.");
+                break;
+        }
+    }
+
+    static InternetConnectionStatus CheckInternetConnection()
+    {
+        try
+        {
+            Ping ping = new Ping();
+            PingReply reply = ping.Send("www.google.com", 3000);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                return InternetConnectionStatus.Connected;
+            }
+            else
+            {
+                Console.WriteLine($"Ping failed with status: {reply.Status}");
+                return InternetConnectionStatus.NotConnected;
+            }
+        }
+        catch (PingException ex)
+        {
+            Console.WriteLine($"Exception during ping: {ex.Message}");
+            return InternetConnectionStatus.Unknown;
+        }
+    }
+}
+
+enum InternetConnectionStatus
+{
+    Connected,
+    NotConnected,
+    Unknown
+}
--- varibales/varibales/Program.cs
+++ varibales/varibales/Program.cs
@@ -6,13 +6,13 @@
         {
             int num1 = 10;
             int num2 = 20;
-
             Console.WriteLine(num1*num2);
 
             string myName = "danies";
             string message = "My name is"+ " " + myName;
             string capMessage = message.ToUpper();
             Console.WriteLine(capMessage);
+            Console.ReadLine();
         }
     }
 }
