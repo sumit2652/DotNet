6030478467911661afd8cb96726d0ce3db973ad0
sumit2652 <sumit_21@outlook.com>
2023-12-26 20:55:27 +0000

    varibale example

--- OOP_C#/OOP_C#/car.cs
+++ OOP_C#/OOP_C#/car.cs
@@ -14,7 +14,6 @@ namespace OOP_C_
         private string _name;
         private int _hp;
         private string _colour;
- 
         public car(string name, int hp = 0, string colour = "Black") 
         {
             _name = name;
@@ -22,21 +21,17 @@ namespace OOP_C_
             _hp = hp;
             _colour = colour;
         }
-
         public void Drive ()
         {
             Console.WriteLine(_name + "Car is driving");
         }
-
         public void Stop()
         {
-            Console.WriteLine(_name + "Car is stopped");
+            Console.WriteLine(_name + " Car is stopped ");
         }
         public void Details()
         {
             Console.WriteLine($"The {_colour} car {_name} has {_hp} hp");
         }
-
-
     }
 }
---
+++ varibales/varibales/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace varibales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            num1 = 13;
+            int num2 = 23;
+
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+    }
+}
